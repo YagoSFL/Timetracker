367e8372cb8a0f769869e5ca365232ea84325f54
YagoSFL <yago_cqs@hotmail.com>
2021-12-23 21:02:18 -0300

    Update Game.tsx, HoursLog.tsx, and 2 more files...

---
+++ src/pages/HoursLog.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HoursLog = () => (
+  <h1>HoursLog</h1>
+);
+
+export default HoursLog;
---
+++ src/pages/TrackingList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TrackingList = () => (
+  <h1>TrackingList</h1>
+);
+
+export default TrackingList;
